5916e9a6b0e0efae110f3740905d7e97451fa757
mno. <mnologics@gmail.com>
2026-01-07 13:52:01 +0900

    A tour of Go/Concurrency

---
+++ exercise-equivalent-binary-trees.go
@@ -0,0 +1,41 @@
+package main
+
+import "golang.org/x/tour/tree"
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := 0; i < 10; i++ {
+		if <-ch1 != <-ch2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	// Example usage:
+	t1 := tree.New(1)
+	t2 := tree.New(1)
+	t3 := tree.New(2)
+
+	println(Same(t1, t2)) // true
+	println(Same(t1, t3)) // false
+}
